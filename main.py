f1f219ef442ed30876cefb0e6f679d41eb64949b
Vasinger <laweczka9@gmail.com>
2025-04-08 03:23:55 +0200

    added sys.argv

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
 from stats import get_characters_count, get_word_count, get_sorted_list
+import sys
+
+#sys.exit("Usage: python3 main.py <path_to_book>")
+
 
 def get_book_text(file_path):
     with open(file_path) as f:
@@ -6,11 +10,15 @@ def get_book_text(file_path):
     return file_contents
 
 def main():
-    count = get_word_count(get_book_text("./books/frankenstein.txt"))
-    char = get_characters_count(get_book_text("./books/frankenstein.txt"))
+    if len(sys.argv) != 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    path = sys.argv[1]
+    count = get_word_count(get_book_text(path))
+    char = get_characters_count(get_book_text(path))
     format = get_sorted_list(char)
     print("============ BOOKBOT ============")
-    print("Analyzing book found at books/frankenstein.txt...")
+    print(f"Analyzing book found at {path}...")
     print("----------- Word Count ----------")
     print(f"Found {count} total words")
     print("--------- Character Count -------")
